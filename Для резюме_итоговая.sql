a7e7fc302e892f3a7c00cab2bf04b3743652972b
Zotov NV <Stifko@mail.ru>
2024-08-12 13:31:37 +0700

    кодировка_2

--- Для резюме_итоговая.sql
+++ Для резюме_итоговая.sql
@@ -1,24 +1,24 @@
---Èòîãîâàÿ ðàáîòà ïî SQL îáû÷íîìó
+--Г€ГІГ®ГЈГ®ГўГ Гї Г°Г ГЎГ®ГІГ  ГЇГ® SQL Г®ГЎГ»Г·Г­Г®Г¬Гі
 
--- Çàäàíèå ¹1==============================================================================================================
+-- Г‡Г Г¤Г Г­ГЁГҐ В№1==============================================================================================================
 with cte_seat as 
 (
 	select 
 		aircraft_code, 
-		count(aircraft_code) as "Êîëëè÷åñòâî ìåñò"
+		count(aircraft_code) as "ГЉГ®Г«Г«ГЁГ·ГҐГ±ГІГўГ® Г¬ГҐГ±ГІ"
 	from seats s
 	group by aircraft_code
 ) 
 select 
 	a.model,
-	"Êîëëè÷åñòâî ìåñò" 
+	"ГЉГ®Г«Г«ГЁГ·ГҐГ±ГІГўГ® Г¬ГҐГ±ГІ" 
 	from aircrafts a 
 		join cte_seat s on a.aircraft_code = s.aircraft_code
-where "Êîëëè÷åñòâî ìåñò" > 50
+where "ГЉГ®Г«Г«ГЁГ·ГҐГ±ГІГўГ® Г¬ГҐГ±ГІ" > 50
 
---Çàäàíèå ¹2================================================================================================================
+--Г‡Г Г¤Г Г­ГЁГҐ В№2================================================================================================================
 
-with price as --Âàðèàíò 1
+with price as --Г‚Г Г°ГЁГ Г­ГІ 1
 (	
 	select 
 		flight_id, 
@@ -33,7 +33,7 @@ with price as --Âàðèàíò 1
 (
 	select 
 		flight_id, 
-		min(min_business) as "Áèçíåññ", max(max_economy) as "Ýêîíîì" from price
+		min(min_business) as "ГЃГЁГ§Г­ГҐГ±Г±", max(max_economy) as "ГќГЄГ®Г­Г®Г¬" from price
 	group by flight_id
 	having max(max_economy) is not null and  min(min_business) is not null
 )
@@ -43,12 +43,12 @@ with price as --Âàðèàíò 1
 	from fli fl
 		join flights f on fl.flight_id = f.flight_id 
 		join airports a on f.arrival_airport = a.airport_code
-	where "Áèçíåññ" < "Ýêîíîì"
+	where "ГЃГЁГ§Г­ГҐГ±Г±" < "ГќГЄГ®Г­Г®Г¬"
 				
 
 
 
-with mas as --Âàðèàíò 2
+with mas as --Г‚Г Г°ГЁГ Г­ГІ 2
 (
 	select 
 		flight_id, 
@@ -62,7 +62,7 @@ with mas as --Âàðèàíò 2
 	select 
 		flight_id, 
 		fare_conditions, 
-		min(amount) as "Áèçíåññ" 
+		min(amount) as "ГЃГЁГ§Г­ГҐГ±Г±" 
 	from mas 
 	where fare_conditions = 'Business'
 	group by 1,2
@@ -72,7 +72,7 @@ with mas as --Âàðèàíò 2
 	select 
 		flight_id, 
 		fare_conditions, 
-		max(amount) as "Ýêîíîì" 
+		max(amount) as "ГќГЄГ®Г­Г®Г¬" 
 	from mas
 	where fare_conditions = 'Economy'
 	group by 1,2
@@ -81,8 +81,8 @@ with mas as --Âàðèàíò 2
 (
 	select 
 		b.flight_id, 
-		b.Áèçíåññ, 
-		e.Ýêîíîì 
+		b.ГЃГЁГ§Г­ГҐГ±Г±, 
+		e.ГќГЄГ®Г­Г®Г¬ 
 	from bus b
 		join eco e on e.flight_id = b.flight_id 
 )
@@ -92,10 +92,10 @@ select
 from fli fl
 	join flights f on fl.flight_id = f.flight_id 
 	join airports a on f.arrival_airport = a.airport_code
-where "Áèçíåññ" < "Ýêîíîì"
+where "ГЃГЁГ§Г­ГҐГ±Г±" < "ГќГЄГ®Г­Г®Г¬"
 
 
---Çàäàíèå ¹3========================================================================================================================
+--Г‡Г Г¤Г Г­ГЁГҐ В№3========================================================================================================================
 
 with bus as 
 (
@@ -111,7 +111,7 @@ select
 from flights f
 	join bus b on f.flight_id = b.flight_id
 	
---Çàäàíèå ¹4========================================================================================================================
+--Г‡Г Г¤Г Г­ГЁГҐ В№4========================================================================================================================
 
 with a as 
 (
@@ -119,14 +119,14 @@ select
 	a.flight_id,
 	a.actual_departure,
 	a.airport_name,
-	"Âñåãî ïîñàäî÷íûõ ìåñò"
+	"Г‚Г±ГҐГЈГ® ГЇГ®Г±Г Г¤Г®Г·Г­Г»Гµ Г¬ГҐГ±ГІ"
  from 
 (
 	select 
 		f.flight_id,
 		a2.airport_name,
 		f.actual_departure,
-		count(s.seat_no)over(partition by f.flight_id) as "Âñåãî ïîñàäî÷íûõ ìåñò" 
+		count(s.seat_no)over(partition by f.flight_id) as "Г‚Г±ГҐГЈГ® ГЇГ®Г±Г Г¤Г®Г·Г­Г»Гµ Г¬ГҐГ±ГІ" 
 	from flights f
 			join aircrafts ai on f.aircraft_code  = ai.aircraft_code 
 			join seats s on ai.aircraft_code = s.aircraft_code
@@ -138,47 +138,47 @@ b as
 (
 select                             
 	b.flight_id,
-	"çàíÿòûõ ìåñò â ñàìîëåòå"  
+	"Г§Г Г­ГїГІГ»Гµ Г¬ГҐГ±ГІ Гў Г±Г Г¬Г®Г«ГҐГІГҐ"  
 from    
 (
 	select 
 		f.flight_id,
-		count(ticket_no) over (partition by f.flight_id) as "çàíÿòûõ ìåñò â ñàìîëåòå"
+		count(ticket_no) over (partition by f.flight_id) as "Г§Г Г­ГїГІГ»Гµ Г¬ГҐГ±ГІ Гў Г±Г Г¬Г®Г«ГҐГІГҐ"
 	from flights f 
 			left join boarding_passes bp on bp.flight_id = f.flight_id
 ) as b
-group by b.flight_id,"çàíÿòûõ ìåñò â ñàìîëåòå"
+group by b.flight_id,"Г§Г Г­ГїГІГ»Гµ Г¬ГҐГ±ГІ Гў Г±Г Г¬Г®Г«ГҐГІГҐ"
 )
 select 
 	a.flight_id,
 	a.actual_departure::date,
 	a.airport_name,
-	a."Âñåãî ïîñàäî÷íûõ ìåñò",
-	b."çàíÿòûõ ìåñò â ñàìîëåòå",
-	round((b."çàíÿòûõ ìåñò â ñàìîëåòå"/a."Âñåãî ïîñàäî÷íûõ ìåñò"::numeric)*100, 2) as procent,
-	sum(b."çàíÿòûõ ìåñò â ñàìîëåòå")over(partition by a.airport_name order by a.actual_departure)
+	a."Г‚Г±ГҐГЈГ® ГЇГ®Г±Г Г¤Г®Г·Г­Г»Гµ Г¬ГҐГ±ГІ",
+	b."Г§Г Г­ГїГІГ»Гµ Г¬ГҐГ±ГІ Гў Г±Г Г¬Г®Г«ГҐГІГҐ",
+	round((b."Г§Г Г­ГїГІГ»Гµ Г¬ГҐГ±ГІ Гў Г±Г Г¬Г®Г«ГҐГІГҐ"/a."Г‚Г±ГҐГЈГ® ГЇГ®Г±Г Г¤Г®Г·Г­Г»Гµ Г¬ГҐГ±ГІ"::numeric)*100, 2) as procent,
+	sum(b."Г§Г Г­ГїГІГ»Гµ Г¬ГҐГ±ГІ Гў Г±Г Г¬Г®Г«ГҐГІГҐ")over(partition by a.airport_name order by a.actual_departure)
 from a 
 	join b on a.flight_id = b.flight_id
 
---Çàäàíèå ¹5========================================================================================================
+--Г‡Г Г¤Г Г­ГЁГҐ В№5========================================================================================================
 
 	select distinct 
-		concat(departure_airport,'-', arrival_airport) as "Ìàðøðóò",
-		count(flight_id)over(partition by concat(departure_airport,'-', arrival_airport)) as "Ïåðåëåòû ïî ìàðøðóòàì",
-		count(flight_id)over() as "Îáùåå êîë-âî ïåðåëåòîâ",
+		concat(departure_airport,'-', arrival_airport) as "ГЊГ Г°ГёГ°ГіГІ",
+		count(flight_id)over(partition by concat(departure_airport,'-', arrival_airport)) as "ГЏГҐГ°ГҐГ«ГҐГІГ» ГЇГ® Г¬Г Г°ГёГ°ГіГІГ Г¬",
+		count(flight_id)over() as "ГЋГЎГ№ГҐГҐ ГЄГ®Г«-ГўГ® ГЇГҐГ°ГҐГ«ГҐГІГ®Гў",
 		round((count(flight_id)over(partition by concat(departure_airport,'-', arrival_airport))/count(flight_id)over()::numeric)*100, 2) 
 	from flights f 
 
 
---Çàäàíèå ¹6=======================================================================================================
+--Г‡Г Г¤Г Г­ГЁГҐ В№6=======================================================================================================
 	
 	select
-		left(split_part(contact_data::text,'"phone": "+7', 2),3) as "Êîä îïåðàòîðà",
-		count(left(split_part(contact_data::text,'"phone": "+7', 2),3)) as "Ïàññàæèðîâ"
+		left(split_part(contact_data::text,'"phone": "+7', 2),3) as "ГЉГ®Г¤ Г®ГЇГҐГ°Г ГІГ®Г°Г ",
+		count(left(split_part(contact_data::text,'"phone": "+7', 2),3)) as "ГЏГ Г±Г±Г Г¦ГЁГ°Г®Гў"
 	from tickets t
 	group by left(split_part(contact_data::text,'"phone": "+7', 2),3)
 	
---Çàäàíèå ¹7=======================================================================================================
+--Г‡Г Г¤Г Г­ГЁГҐ В№7=======================================================================================================
 with tab as
 (
 	select
@@ -194,22 +194,22 @@ with tab as
 	from flights f 
 )
 select
-	tab.departure as "Àýðîïîðò îòïðàâëåíèÿ",
-	tab.arrival as "Àýðîïîðò ïðèáûòèÿ",
-	a2.city as "Ãîðîä îòïðàâëåíèÿ",
-	a.city as "Ãîðîä ïðèáûòèÿ",
-	concat(a2.city, ' - ', a.city) as "Ìàðøðóò"
+	tab.departure as "ГЂГЅГ°Г®ГЇГ®Г°ГІ Г®ГІГЇГ°Г ГўГ«ГҐГ­ГЁГї",
+	tab.arrival as "ГЂГЅГ°Г®ГЇГ®Г°ГІ ГЇГ°ГЁГЎГ»ГІГЁГї",
+	a2.city as "ГѓГ®Г°Г®Г¤ Г®ГІГЇГ°Г ГўГ«ГҐГ­ГЁГї",
+	a.city as "ГѓГ®Г°Г®Г¤ ГЇГ°ГЁГЎГ»ГІГЁГї",
+	concat(a2.city, ' - ', a.city) as "ГЊГ Г°ГёГ°ГіГІ"
 from tab 
 	join airports a on tab.arrival = a.airport_code 
 	join airports a2 on tab.departure = a2.airport_code
 	
---Çàäàíèå ¹8=======================================================================================================
+--Г‡Г Г¤Г Г­ГЁГҐ В№8=======================================================================================================
 
 with route as 
 (
 	select 
-		concat(f.departure_airport, ' - ', f.arrival_airport) as "Ìàðøðóò",
-		sum(amount) as "Ñóììà"
+		concat(f.departure_airport, ' - ', f.arrival_airport) as "ГЊГ Г°ГёГ°ГіГІ",
+		sum(amount) as "Г‘ГіГ¬Г¬Г "
 	from flights f
 		join ticket_flights tf on tf.flight_id = f.flight_id
 	group by concat(f.departure_airport, ' - ', f.arrival_airport)
@@ -217,20 +217,20 @@ with route as
 	classific as
 (
 	select 
-		"Ìàðøðóò",
-		"Ñóììà",
-		case when 0 < "Ñóììà"  and "Ñóììà" < 50000000 then 'low'
-			 when "Ñóììà" >= 50000000 and "Ñóììà" < 150000000 then 'middle' 
+		"ГЊГ Г°ГёГ°ГіГІ",
+		"Г‘ГіГ¬Г¬Г ",
+		case when 0 < "Г‘ГіГ¬Г¬Г "  and "Г‘ГіГ¬Г¬Г " < 50000000 then 'low'
+			 when "Г‘ГіГ¬Г¬Г " >= 50000000 and "Г‘ГіГ¬Г¬Г " < 150000000 then 'middle' 
 			 else 'high' end "Class"	
 	from route
 )
 select 
 	"Class",
-	count("Ìàðøðóò")
+	count("ГЊГ Г°ГёГ°ГіГІ")
 from classific
 group by "Class"
 
---Çàäàíèå ¹9======================================================================================================
+--Г‡Г Г¤Г Г­ГЁГҐ В№9======================================================================================================
 
 select * from airports a 
 
@@ -249,6 +249,6 @@ with city as
 )
 select 
 	concat(departure, ' - ', arrival),
-	round(6371*(acos(sin(latitude_a)*sin(latitude_b) + cos(latitude_a)*cos(latitude_b)*(cos(longitude_a - longitude_b))))::numeric, 0) as "Ðàññòîÿíèå ìåæäó ãîðîäàìè"
+	round(6371*(acos(sin(latitude_a)*sin(latitude_b) + cos(latitude_a)*cos(latitude_b)*(cos(longitude_a - longitude_b))))::numeric, 0) as "ГђГ Г±Г±ГІГ®ГїГ­ГЁГҐ Г¬ГҐГ¦Г¤Гі ГЈГ®Г°Г®Г¤Г Г¬ГЁ"
 from city
 where round(6371*(acos(sin(latitude_a)*sin(latitude_b) + cos(latitude_a)*cos(latitude_b)*(cos(longitude_a - longitude_b))))::numeric, 0) > 5000
